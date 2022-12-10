fd89c051bf27ebfe84b8b07b6fbf923e74fc5d90
Justin Jones <jus.jones@yahoo.com>
2022-12-09 22:17:15 -0500

    updating files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import {Route, Switch } from 'react-router-dom'
 
 function App() {
+
+  const [pickUpLines, setPickUpLines] = useState([])
+  const [successStories, setSuccessStories] = useState([])
+  const url = ("http://localhost:3004/pickuplines")
+  const urlFeedBack = ("http://localhost:3004/feedback")
+  
+
   return (
     <div className="App">
+
     
     </div>
   );
