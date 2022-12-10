52fd86b6f7699e08e9a2b1f3b1806fa4e48ff375
Justin Jones <jus.jones@yahoo.com>
2022-12-09 22:35:09 -0500

    updating app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,13 +8,41 @@ function App() {
   const [successStories, setSuccessStories] = useState([])
   const url = ("http://localhost:3004/pickuplines")
   const urlFeedBack = ("http://localhost:3004/feedback")
-  
+
+  useEffect(()=>{
+    fetch(url)
+    .then(res => res.json())
+    .then(data => setPickUpLines(data))
+  }, [])
+
+  useEffect(()=>{
+
+    fetch(urlFeedBack)
+    .then(res => res.json())
+    .then(data => setSuccessStories(data))
+
+ },[])
 
   return (
-    <div className="App">
+    <div>
+    <Nav />
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/newpickupline">
+        <NewPickUpLineForm handleNewLine={handleNewLine} />
+      </Route>
+      <Route exact path="/success">
+        <Success successStories={successStories}/>
+      </Route>
 
-    
-    </div>
+      <Route exact path="/allpickuplines">
+        <AllPickUpLines pickUpLines={pickUpLines}  />
+      </Route>
+      {/* <PickUpLinesContainer pickUpLines={pickUpLines}/> */}
+    </Switch>
+     </div>
   );
 }
 
